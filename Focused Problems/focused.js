be8a481e237efdb2348e53e8d79c25d4c9dee102
tiffanyw-hu <tiffanyw.hu@gmail.com>
2018-12-16 23:23:41 -0800

    problem 4

--- Focused Problems/focused.js
+++ Focused Problems/focused.js
@@ -39,3 +39,31 @@ function isPalindrome(str) {
   }
   return true
 }
+
+function isPalindrome(str) {
+  let mid = str.length / 2
+  let j = str.length - 1
+  while (j <= mid) {
+    for (let i = 0; i <= mid; i++) {
+      if (str[i] !== str[j]) {
+        return false
+      }
+      j--;
+    }
+  }
+  return true
+}
+
+function isPalindrome(str) {
+  let mid = str.length / 2
+  let j = str.length - 1
+  while (j <= mid) {
+    for (let i = 0; i <= mid; i++) {
+      if (str[i] !== str[j]) {
+        return false
+      }
+      j--;
+    }
+  }
+  return true
+}
